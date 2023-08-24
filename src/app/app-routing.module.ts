345c44064da36e08dc9cb1d0987001ed5e5f860f
Darwin0703 <djoplin0703@gmail.com>
2023-08-23 23:14:02 -0500

    Se cargan App con componentes y estilos ya generados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { PersonajeComponent } from './components/personaje/personaje.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { NavesComponent } from './components/naves/naves.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
+import { EspeciesComponent } from './components/especies/especies.component';
+import { PlanetasComponent } from './components/planetas/planetas.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'personajes', component: PersonajesComponent},
+  {path: 'people', component: PersonajeComponent},
+  {path: 'peliculas', component: PeliculasComponent},
+  {path: 'naves', component: NavesComponent},
+  {path: 'vehiculos', component: VehiculosComponent},
+  {path: 'especies', component: EspeciesComponent},
+  {path: 'planetas', component: PlanetasComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavesComponent } from './components/naves/naves.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PersonajeComponent } from './components/personaje/personaje.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { PlanetasComponent } from './components/planetas/planetas.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
+import { EspeciesComponent } from './components/especies/especies.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SearchComponent } from './components/shared/search/search.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EspeciesComponent,
+    HomeComponent,
+    NavesComponent,
+    PeliculasComponent,
+    PersonajeComponent,
+    PersonajesComponent,
+    PlanetasComponent,
+    VehiculosComponent,
+    LoadingComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
